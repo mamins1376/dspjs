6465ef9c4f9b5af9006e8cf040a1d7fe9bda68e3
Mohammad Amin Sameti <mamins1376@gmail.com>
2021-07-13 00:52:54 +0430

    Better looking spectrogram

--- src/audio.ts
+++ src/audio.ts
@@ -372,11 +372,24 @@ class SpectrogramVisualiser implements Visualiser {
     const { data } = this.data;
     data.copyWithin(context.canvas.width << 2, 0);
     for (const [i, v] of this.buffer.entries()) {
-      data[i << 2] = v;
+      const l = v / 255.0, h = l / 6;
+      const q = l < 0.5 ? l * 2 : 1, p = 2 * l - q;
+      data[i << 2 + 0] = 255 * hue2rgb(p, q, h + 1 / 3);
+      data[i << 2 + 1] = 255 * hue2rgb(p, q, h);
+      data[i << 2 + 2] = 255 * hue2rgb(p, q, h - 1 / 3);
     }
 
     this.context.putImageData(this.data, 0, 0);
   }
+}
+
+function hue2rgb(p: number, q: number, t: number): number {
+  if (t < 0) t += 1;
+  if (t > 1) t -= 1;
+  if (t < 1 / 6) return p + (q - p) * 6 * t;
+  if (t < 1 / 2) return q;
+  if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+  return p;
 }
 
 async function makeEffectNode(context: AudioContext): Promise<EffectNode> {
